8ecc6781bdade40f571c04f0ddf19f99ac46c728
Antonio Salvia <antonio.salvia@students.fhnw.ch>
2024-05-08 09:47:01 +0200

    added fake data, first simple Issues view

--- app/Issues/page.tsx
+++ app/Issues/page.tsx
@@ -1,7 +1,45 @@
+import issues from '../data/issues.json' assert { type: 'json' };;
+import styles from '../issues.module.css'
+
+function List({ list }: { list: SystemMonitoringIssue[] }) {
+    return (
+        <div className={styles.issueList}>
+            {list.slice(1).map(issue => (
+                <div key={issue.id} className={styles.issueCard}>
+                    <h2>{issue.title}</h2>
+                    <p>{issue.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+
+function MainCard({ firstIssue }: { firstIssue: SystemMonitoringIssue }) {
+    return (
+        <div className={styles.mainIssue} >
+            <h1>{firstIssue.title}</h1>
+            <p>{firstIssue.description}</p>
+        </div>
+    )
+}
+
+const systemMonitoringIssuesArray: SystemMonitoringIssue[] = issues.map(issue => {
+    return {
+        ...issue,
+        timestamp: new Date(issue.timestamp),
+        endTime: new Date(issue.endTime),
+        lastUpdated: new Date(issue.lastUpdated),
+    };
+});
+
 export default function IssuesPage() {
     return (
-        <section>
-            <h1>This is a test</h1>
-        </section>
+        <div>
+            <div className={styles.app}>
+                <MainCard firstIssue={systemMonitoringIssuesArray[0]} />
+                <List list={systemMonitoringIssuesArray} />
+            </div>
+        </div>
     )
 }
---
+++ app/data/data.tsx
@@ -0,0 +1,20 @@
+interface SystemMonitoringIssue {
+    id: number,
+    title: string;
+    description: string;
+    status: string;//'Open' | 'Closed' | 'In Progress';
+    timestamp: Date;
+    creator: string;
+    duration: number;
+    endTime: Date;
+    lastUpdated: Date;
+    impact: string;
+    affectedSystems: string[];
+    alertType: string;//'Warning' | 'Info' | 'Critical';
+    incidentType: string;//'Performance' | 'Storage' | 'Overheating' | 'Backups' | 'Power';
+    priority: number;
+    severity: string;//'Low' | 'Medium' | 'High';
+    preventativeMeasures: string;
+    attachments: string[];
+    comments: string[];
+}
