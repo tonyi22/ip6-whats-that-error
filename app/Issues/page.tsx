ab8a07e0d2bb7c34d0910d5495ba244626ec78be
Antonio Salvia <antonio.salvia@students.fhnw.ch>
2024-08-01 10:31:25 +0200

    change high to german

--- app/Issues/page.tsx
+++ app/Issues/page.tsx
@@ -119,7 +119,7 @@ function CardsHeader({ onSort, sortColumn, sortDirection }: CardsHeaderProps) {
                 {true &&
                     <th className="px-4 py-3 text-start text-xs font-medium text-gray-800 uppercase dark:text-neutral-500 w-40" onClick={() => onSort('severity')}>
                         <div className='flex items-center'>
-                            <Tippy theme="tomato-theme" content={<span><span className="font-bold" >Schweregrad<br /></span><span className="font-bold text-green-500">Low:</span> Niedrig<br /><span className="font-bold text-yellow-500">Medium:</span> Mittel<br /><span className="font-bold text-red-500">Rot:</span> High</span>}>
+                            <Tippy theme="tomato-theme" content={<span><span className="font-bold" >Schweregrad<br /></span><span className="font-bold text-green-500">Low:</span> Niedrig<br /><span className="font-bold text-yellow-500">Medium:</span> Mittel<br /><span className="font-bold text-red-500">Rot:</span> Hoch</span>}>
                                 <div className='flex items-center'>Severity {renderSortIcon('severity')}</div>
                             </Tippy>
                         </div>
@@ -391,7 +391,7 @@ function Card1({ id, heading, description, icon, className = '', priority, times
                 <button
                     className="bg-blue-500 text-white px-4 py-3 rounded hover:bg-blue-600"
                     onClick={handleDismiss}>
-                    Mark as read
+                    Gelesen
                 </button>
 
             </div>
